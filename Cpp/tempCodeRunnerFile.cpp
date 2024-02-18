f2bff73bc9f9bd40a43c8170fa91b3bb883cc891
alhad-balak <abhinav.iit19@gmail.com>
2024-02-18 23:57:36 +0530

    Updated Cpp

--- Cpp/tempCodeRunnerFile.cpp
+++ Cpp/tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-for (auto it : pmp)
-    {
-        cout << it.first << ": ";
-        for (auto iter : it.second)
-            cout << iter << " ";
-        cout << endl;
-    }
-    cout << endl;
-    for (auto it : smp)
-    {
-        cout << it.first << ": ";
-        for (auto iter : it.second)
-            cout << iter << " ";
-    }
+
